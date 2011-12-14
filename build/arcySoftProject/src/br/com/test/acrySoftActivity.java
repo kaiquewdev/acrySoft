17fc2ec1d5684ddd2331846dee5578c0da90fbae
Kaique Silva <kaiquesilva@visie.com.br>
2011-12-14 01:34:37 -0200

    Acry Soft
    Projeto iniciado e testado com a primeira compilacao

---
+++ build/arcySoftProject/src/br/com/test/acrySoftActivity.java
@@ -0,0 +1,15 @@
+package br.com.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class acrySoftActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
